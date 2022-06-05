301987093c38c99d80bcc7540e20065741cb4f53
Jakub Karwatka <jakubkarwatka@gmail.com>
2022-06-05 23:02:01 +0200

    Add fonts, styling and basic components

---
+++ components/atoms/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+  font-family: 'Bellefair';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 150px;
+  line-height: 172px;
+  color: #FFFFFF;
+`
+
+export const H2 = styled.h2`
+  font-family: 'Bellefair';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 100px;
+  line-height: 115px;
+  color: #FFFFFF;
+`
+
+export const H3 = styled.h3`
+  font-family: 'Bellefair';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 56px;
+  line-height: 64px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+`
+
+export const H4 = styled.h4`
+  font-family: 'Bellefair';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 37px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+`
+
+export const H5 = styled.h5`
+  font-family: 'Barlow Condensed';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 34px;
+  letter-spacing: 4.725px;
+  color: #D0D6F9;
+`
+export const SubHeading1 = styled.h6`
+  font-family: 'Bellefair';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 32px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+`
+
+export const SubHeading2 = styled.h6`
+  font-family: 'Barlow Condensed';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 2.3625px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+`
+
+export const NavText = styled.p`
+  font-family: 'Barlow Condensed';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: 2.7px;
+  color: #FFFFFF;
+`
+
+export const BodyText = styled.p`
+  font-family: 'Barlow';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 32px;
+  color: #FFFFFF;
+`
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { H1 } from '../components/atoms'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () =>  (
-    <h1>Space Tourism</h1>
+    <H1>Space Tourism</H1>
   )
 
 
