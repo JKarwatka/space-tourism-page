4a47347cd9291bed5450a40bf856a90a1a271829
Jakub Karwatka <jakubkarwatka@gmail.com>
2022-10-11 22:17:02 +0200

    Add assets for  destination  pagee

---
+++ pages/destination/content.ts
@@ -0,0 +1,33 @@
+export const destinationPageContent = {
+  PAGE_TITLE: "Pick your destination",
+  AVG_DISTANCE_TITLE: "Avg. distance",
+  TRAVEL_TIME_TITLE: "Est. travel time"
+}
+
+
+export const planetContent = {
+  MOON: {
+    PLANET_NAME: "Moon",
+    PLANET_DESCRIPTION: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+    AVG_DISTANCE: "384,400 km",
+    TRAVEL_TIME:  "3 days",
+  },
+  MARS: {
+    PLANET_NAME: "Mars",
+    PLANET_DESCRIPTION: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+    AVG_DISTANCE: "225 MIL. km",
+    TRAVEL_TIME:  "9 months",
+  },
+  EUROPA: {
+    PLANET_NAME: "Europa",
+    PLANET_DESCRIPTION: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+    AVG_DISTANCE: "628 MIL. km",
+    TRAVEL_TIME:  "3 years",
+  },
+  TITAN: {
+    PLANET_NAME: "Titan",
+    PLANET_DESCRIPTION: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
+    AVG_DISTANCE: "1.6 BIL. km",
+    TRAVEL_TIME:  "7 years",
+  }
+}
